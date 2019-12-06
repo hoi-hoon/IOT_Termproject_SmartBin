bba23af86ee3df1555eb6daf7aee474abbb5d0fb
hoi-hoon <43691105+hoi-hoon@users.noreply.github.com>
2019-12-06 17:58:01 +0900

    inner_senosr

--- smart_bin/smart_bin.ino
+++ smart_bin/smart_bin.ino
@@ -6,35 +6,35 @@ int outer_trig = 8;
 int outer_echo = 9;
 
 int inner_trig = 4;
-int inner_echo = 9;
+int inner_echo = 5;
+
+int full_cnt = 0;
 
 void setup() {
   Serial.begin(9600);  
-  // Out pin Input Setup
-  pinMode(outer_echo, INPUT);
-  // Trig pin Output Setup
-  pinMode(outer_trig, OUTPUT);
   SV.attach(10);
+  pinMode(outer_echo, INPUT);
+  pinMode(outer_trig, OUTPUT);
+  pinMode(inner_echo, INPUT);
+  pinMode(inner_trig, OUTPUT);
+
   digitalWrite(outer_trig, 0);
+  digitalWrite(inner_trig, 0);
 }
 
 void loop() {
   unsigned long microseconds, distance_cm;
-  int full_cnt = 0;
-  // Ultrasonic wave transmission
-  digitalWrite(outer_trig, 0); // Output pin_ULTRASONIC_T to LOW
+  
+  digitalWrite(outer_trig, 0);
   delayMicroseconds(2);
-  digitalWrite(outer_trig, 1); // Output pin_ULTRASONIC_T to HIGH       
+  digitalWrite(outer_trig, 1);       
   delayMicroseconds(10);
-  digitalWrite(outer_trig, 0); // Output pin_ULTRASONIC_T to LOW
+  digitalWrite(outer_trig, 0);
   
   microseconds = pulseIn(outer_echo, 1, 24000);
   distance_cm = microseconds * 17/1000;
-  
-  Serial.print("Distance :");
-  Serial.print(distance_cm);
-  Serial.println("[cm]");
-  if(distance_cm<=10){
+
+  if(distance_cm<=30){
     Serial.println("Trash In");
     SV.write(100);
     delay(3000);
@@ -54,19 +54,21 @@ void loop() {
   microseconds = pulseIn(inner_echo, 1, 24000);
   // time to dist
   distance_cm = microseconds * 17/1000;
-  Serial.print("Inner Distance :");
-  Serial.print(distance_cm);
-  Serial.println("[cm]");
-  if(distance_cm<=10){
+  Serial.print("inner dist:  ");
+  Serial.println(distance_cm);
+  if(distance_cm<=20){
+    
     Serial.println("so closed");
     full_cnt++;
+    Serial.println(full_cnt);
   }
   else{
     full_cnt = 0;
   }
 
-  if(full_cnt>10){
+  if(full_cnt==10){
     Serial.println("-------------- Send Alert -------------");
   }
+  
   delay(1000);
 }
