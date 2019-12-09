42263cd317c7c1f30069a11f343bee03696efe24
hoi-hoon <43691105+hoi-hoon@users.noreply.github.com>
2019-12-09 20:49:43 +0900

    complete

---
+++ bin_pushetta/bin_pushetta.ino
@@ -0,0 +1,66 @@
+#include "WiFiEsp.h"
+
+#ifndef HAVE_HWSERIAL1
+#include "SoftwareSerial.h"
+SoftwareSerial Serial1(6, 7); // RX, TX
+#endif
+
+
+String userKey = "u9gnbfp4r56ob5qqgz1kqbot413vkc";     // your Pushover user key
+String apiToken = "a3bevaxbpens5x7hyai43ms1mcp42o";    // your Pushover API token
+String Title = "ESP8266 Pushover Test";                // your title of the message
+ 
+char ssid[] = "Hoi";                        
+char password[] = "123123123";
+int status = WL_IDLE_STATUS; 
+
+int length;
+
+WiFiEspClient client;
+
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(9600);
+  WiFi.init(&Serial1);
+
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    while (true);
+  }
+}
+ 
+void loop() {
+   if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue
+    while (true);
+  }
+
+  // attempt to connect to WiFi network
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network
+    status = WiFi.begin(ssid, password);
+  }
+
+  Serial.println("You're connected to the network");
+  
+  sendToPushover("* Door OPEN !!! * 좀전에 문이 열렸습니다 *");  
+  while(true);
+}
+ 
+void sendToPushover(String Msg) {
+  Msg = Msg+"&title="+Title;
+  length = Msg.length()+81;
+  if (client.connect("api.pushover.net", 80)) {
+    client.println("POST /1/messages.json HTTP/1.1");
+    client.println("Host: api.pushover.net");
+    client.println("Connection: close\r\nContent-Type: application/x-www-form-urlencoded");
+    client.print("Content-Length: ");
+    client.print(length);
+    client.println("\r\n");
+    client.print("token="+apiToken+"&user="+userKey+"&message="+Msg);
+    client.stop();
+  }
+} 
--- smart_bin/smart_bin.ino
+++ smart_bin/smart_bin.ino
@@ -1,4 +1,10 @@
 #include <Servo.h>
+#include "WiFiEsp.h"
+
+#ifndef HAVE_HWSERIAL1
+#include "SoftwareSerial.h"
+SoftwareSerial Serial1(6, 7); // RX, TX
+#endif
 
 Servo SV;
 
@@ -10,8 +16,27 @@ int inner_echo = 5;
 
 int full_cnt = 0;
 
+String userKey = "u9gnbfp4r56ob5qqgz1kqbot413vkc";     // your Pushover user key
+String apiToken = "a3bevaxbpens5x7hyai43ms1mcp42o";    // your Pushover API token
+String Title = "Smart Bin";                // your title of the message
+ 
+char ssid[] = "Hoi";                        
+char password[] = "123123123";
+int status = WL_IDLE_STATUS; 
+
+int length;
+
+WiFiEspClient client;
+
 void setup() {
-  Serial.begin(9600);  
+  Serial.begin(115200);
+  Serial1.begin(9600);
+  WiFi.init(&Serial1);
+
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    while (true);
+  }
   SV.attach(10);
   pinMode(outer_echo, INPUT);
   pinMode(outer_trig, OUTPUT);
@@ -35,6 +60,7 @@ void loop() {
   distance_cm = microseconds * 17/1000;
 
   if(distance_cm<=30){
+    // Serial.print(distance_cm);
     Serial.println("Trash In");
     SV.write(100);
     delay(3000);
@@ -67,8 +93,35 @@ void loop() {
   }
 
   if(full_cnt==10){
-    Serial.println("-------------- Send Alert -------------");
+    if (WiFi.status() == WL_NO_SHIELD) {
+      Serial.println("WiFi shield not present");
+      // don't continue
+      while (true);
+    }
+    while ( status != WL_CONNECTED) {
+      Serial.print("Attempting to connect to WPA SSID: ");
+      Serial.println(ssid);
+      // Connect to WPA/WPA2 network
+      status = WiFi.begin(ssid, password);
+    }
+    Serial.println("You're connected to the network");
+    sendToPushover("* Bin is full *");
   }
   
   delay(1000);
 }
+
+void sendToPushover(String Msg){
+  Msg = Msg+"&title="+Title;
+  length = Msg.length()+81;
+  if (client.connect("api.pushover.net", 80)) {
+    client.println("POST /1/messages.json HTTP/1.1");
+    client.println("Host: api.pushover.net");
+    client.println("Connection: close\r\nContent-Type: application/x-www-form-urlencoded");
+    client.print("Content-Length: ");
+    client.print(length);
+    client.println("\r\n");
+    client.print("token="+apiToken+"&user="+userKey+"&message="+Msg);
+    client.stop();
+  }
+} 
